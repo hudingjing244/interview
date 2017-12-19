ed3baacde7cf5a0e03f6794c40c0d44b2d8ece75
潘星 <panxing@imagedt.com>
2017-12-19 15:29:07 +0800

    *

---
+++ src/main/java/com/panxing/interview/InvokeCount.java
@@ -0,0 +1,27 @@
+package com.panxing.interview;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
+
+/**
+ * @author panxing
+ */
+public class InvokeCount {
+
+    private static final AtomicLong COUNTER = new AtomicLong(0L);
+
+    /**
+     * @return 这个方法被调用的总次数，支持并发调用
+     */
+    public static long count() {
+        return COUNTER.incrementAndGet();
+    }
+
+    public static void main(String[] args) {
+        int target = 10000;
+        IntStream.range(1, target).parallel().forEach(value -> count());
+        System.out.println("expect:" + target + ", cal: " + count());
+    }
+
+
+}
